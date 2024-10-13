089efd09dbe8c7ed052081d87517d91f35229317
Martin Fetzel <U01K1RM@post.at>
2024-10-13 18:34:54 +0200

    rename and move

---
+++ Kaggle.BenchmarkRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using Kaggle.BenchmarkRunner;
+
+Console.WriteLine("Hello, World!");
+var config = new ManualConfig()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .AddValidator(JitOptimizationsValidator.DontFailOnError)
+    .AddLogger(ConsoleLogger.Default)
+    .AddColumnProvider(DefaultColumnProviders.Instance);
+var summary = BenchmarkRunner.Run<RunLengthEncodingDecodingBenchmark>(config);
---
+++ Kaggle.BenchmarkRunner/RunLengthEncodingDecodingBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Fourier.ConsoleApp1;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Kaggle.BenchmarkRunner
+{
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    //[SimpleJob(RuntimeMoniker.NativeAot80)]
+    public class RunLengthEncodingDecodingBenchmark
+    {
+        private readonly RunLengthEncodingDecoding runLengthEncoding = new();
+        private Image<Rgba32>? image;
+        private IEnumerable<string>? lines;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var workingDir = @"D:\Kfk-Praktikum\";
+
+            var imagePath = Path.Combine(workingDir, "0114f484a16c152baa2d82fdd43740880a762c93f436c8988ac461c5c9dbe7d5.png");
+            var solutionPath = Path.Combine(workingDir, "stage1_solution.csv");
+
+            lines = File.ReadAllLines(solutionPath).Where(a => a.StartsWith(new FileInfo(imagePath).Name.Replace(".png", "")));
+            image = Image.Load<Rgba32>(imagePath);
+        }
+
+        [Benchmark]
+        public void MarkImageUsingRunLengthEncoding() => runLengthEncoding.MarkImageUsingRunLengthDecoding(lines!, image!, new Rgba32(255, 0, 0));
+
+        [GlobalCleanup()]
+        public void Cleanup_Image() => image!.Dispose();
+    }
+}
